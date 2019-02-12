b782f203c0b9b25dfb5abc8c19a7cd735ab5c0f2
netaro-N <takebbb1+github@gmail.com>
2019-02-12 12:18:18 +0000

    next challenge

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Parse = require('./csvParse');
 const Fixture = require('./models/fixtures');
 Parse.forEach((f,i) =>{
   console.log(i);
+  //保存時はmoment-timeモジュールによって( DD/MM/YYYY HH:hh → YYYY/MM/DD HH:hh )表記へ
   Fixture.upsert({
     gameNumber: f.gameNumber,
     fixtureDate: f.fixtureDate,
